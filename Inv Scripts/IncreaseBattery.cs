b68ae0c3e391b93d75a8621d9ab36a63d519d6f6
Mubashir Rasool Razvi <mrrazvi@icloud.com>
2023-05-27 14:13:08 +0500

    multiline comments

--- Inv Scripts/IncreaseBattery.cs
+++ Inv Scripts/IncreaseBattery.cs
@@ -2,6 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+IncreaseBattery Class
+
+This class represents a battery boost pick-up item in a Unity game.
+When the player interacts with the battery boost pick-up, it adds battery boosts to their inventory.
+
+Public Variables:
+    - _addBatteryBoostToInventory: The number of battery boosts to add to the inventory.
+    - _batteryBoostPickUpActivated: Indicates if the battery boost pick-up has been activated.
+
+Private Variables:
+    None.
+
+Methods:
+    - Start(): Initializes the _batteryBoostPickUpActivated variable.
+    - PickUpBatteryBoost(): Adds battery boosts to the player's inventory when picked up.
+
+Author: Mubashir Rasool Razvi
+Date: May 2023
+*/
+
+
 public class IncreaseBattery : MonoBehaviour
 {
 
