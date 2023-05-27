bfb98bd66fa9f095db678c9ff64c773f473bb6fd
Mubashir Rasool Razvi <mrrazvi@icloud.com>
2023-05-27 14:12:05 +0500

    multiline comments

--- Inv Scripts/BatteryPickUp.cs
+++ Inv Scripts/BatteryPickUp.cs
@@ -2,6 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+BatteryPickUp Class
+
+This class represents a battery pick-up item in a Unity game.
+When the player interacts with the battery pick-up, it adds batteries to their inventory.
+
+Public Variables:
+    - _addBatteriesToInventory: The number of batteries to add to the inventory.
+    - _batteryPickUpActivated: Indicates if the battery pick-up has been activated.
+
+Private Variables:
+    None.
+
+Methods:
+    - Start(): Initializes the _batteryPickUpActivated variable.
+    - PickUpBattery(): Adds batteries to the player's inventory when picked up.
+
+Author: Mubashir Rasool Razvi
+Date: May 2023
+*/
+
 public class BatteryPickUp : MonoBehaviour
 {
     public int _addBatteriesToInventory = 1;
